a155a79da393423b5ded24a86a79dafb4e45edbc
NikkariAoe <diaokx@qq.com>
2017-05-09 20:24:10 +0800

    Character_ver1

---
+++ Character/Enemy_v1.hpp
@@ -0,0 +1,14 @@
+#ifndef ENEMY_HPP_
+#define ENEMY_HPP_
+
+#include "Character.hpp"
+
+class Enemy :public Character {
+    public:
+        Enemy() { std::cout << "Enemy!" << std::endl; }
+        virtual void move();
+        virtual void shoot();
+        virtual void beShooted();
+};
+
+#endif
