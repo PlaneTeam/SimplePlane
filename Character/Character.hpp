d2ffee62d8f53aa76c9efd03d463e8b526c8ac31
NikkariAoe <diaokx@qq.com>
2017-05-15 23:04:09 +0800

    补充了character的成员和函数

--- Character/Character.hpp
+++ Character/Character.hpp
@@ -1,18 +1,27 @@
 #ifndef CHARACTER_HPP_
 #define CHARACTER_HPP_
 
+#define PLAYER_INITIAL_HP 5
+#define ENEMY_INITIAL_HP 3
 class Character {
     public:
-    	enum Bullet{player,enemy};
-        Character(){ std::cout << "Character!" << std::endl; }
+    	enum Bullet { player , enemy };
+        Character() { std::cout << "Character!" << std::endl; }
         virtual ~Character();
+        void setCoordinate(int,int);
+        void setSpeed(int);
+        void setHP(int);
         virtual void move();
         virtual void shoot();
         virtual void beShooted();
-        void isDead();
+        virtual void isDead();
     private:
+    	int x;
+    	int y;
     	int HP;
+    	int speed;
     	enum Bullet bullet;
+    	bool is_dead;
 };
 
 #endif
